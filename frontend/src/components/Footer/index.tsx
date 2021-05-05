24826b74ac070b51839b4f34df23e8848fe8cf37
Carlos Tadeu <tadeucarlos07@gmail.com>
2021-05-05 14:08:16 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/CarlosTadeu" target="_blank"
+                                                                  rel="noreferrer">Carlos Tadeu</a></p>
+                <p className="text-light"><small><strong>Projeto Spring React</strong><br/>
+                    <a href="https://instagram.com/carlostadeu.class"
+                       target="_blank"
+                       rel="noreferrer">@carlostadeu.class
+                    </a></small>
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
